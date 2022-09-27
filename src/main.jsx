2c83e6c5d0653b43faea7d90145f66e8dfcb49ab
Madhukesh K T <madhukeshthakur@gmail.com>
2022-09-27 17:51:01 +0530

    master branch - initial commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
+)
