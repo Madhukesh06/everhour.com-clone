38324b5c96786998a06f8fcfdaa105b474e59678
iammostak <mdmostafijur85@gmail.com>
2022-09-28 09:33:50 +0530

    Demo page linked

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import Demo from "../components/Demo/Demo";
 
 const AllRoutes = () => {
    return (
@@ -6,7 +7,7 @@ const AllRoutes = () => {
          <Route path='/' element={<h1>Home page</h1>} />
          <Route path='/login' element={<h1>Log in</h1>} />
          <Route path='/signup' element={<h1>Signup</h1>} />
-         <Route path='/demo' element={<h1>Demo</h1>} />
+         <Route path='/demo' element={<Demo />} />
          <Route path='/pricing' element={<h1>Pricing</h1>} />
       </Routes>
    );
